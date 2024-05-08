0dd17c138b81e1f2b8d7baf23e181000f5863342
Cal Schwick <35741152+CalSch@users.noreply.github.com>
2024-05-08 10:51:07 -0500

    added a data box

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ canvas.addEventListener('mousedown',(ev)=>{
 let ctx = canvas.getContext('2d');
 
 let startTime = Date.now();
+let lastClickTime = Date.now();
 let duration = 60;
 
 let bgColor = [255,0,0];
@@ -33,30 +34,78 @@ let shapeRadius = 50;
 let hit = 0;
 let missed = 0;
 
+/**
+ * @typedef {{
+ *  time: number;
+ *  shape: {
+ *      x: number;
+ *      y: number;
+ *      radius: number;
+ *      color: number[];
+ *  };
+ *  bg: number[];
+ *  hit: boolean;
+ * }} DataPoint
+ */
+
+/**
+ * @type {{
+ * 	hitData: DataPoint[],
+ *  hit: number,
+ * 	missed: number,
+ * 	total: number,
+ * }}
+ */
+let data = {
+	hitData: [],
+	hit: 0,
+	missed: 0,
+	total: 0
+};
+
 let started = false;
 
 function generateNewShape() {
-	shapeX = Math.random()*screenWidth;
-	shapeY = Math.random()*screenHeight;
-	shapeRadius = Math.random()*50 + 10;
-	shapeColor = [Math.random()*255,Math.random()*255,Math.random()*255];
-	bgColor = [Math.random()*255,Math.random()*255,Math.random()*255];
+	shapeX = Math.floor(Math.random()*screenWidth);
+	shapeY = Math.floor(Math.random()*screenHeight);
+	shapeRadius = Math.floor(Math.random()*50 + 10);
+	shapeColor = [];
+	bgColor = [];
+	for (let i=3;i--;) {
+		shapeColor.push(Math.floor(Math.random()*255));
+		bgColor.push(Math.floor(Math.random()*255));
+	}
 }
 
 function checkForClick() {
 	if (!started) {
 		startTime = Date.now();
+		lastClickTime = Date.now();
 		started = true;
 	}
 	if (isTimeUp()) {
 		return;
 	}
+	let dataPoint = {
+		time: (Date.now() - lastClickTime)/1000,
+		shape: {
+			x: shapeX,
+			y: shapeY,
+			radius: shapeRadius,
+			color: shapeColor
+		},
+		bg: bgColor,
+		hit: false
+	};
+	lastClickTime = Date.now();
 	let dist = Math.hypot(mouseX-shapeX,mouseY-shapeY);
 	if (dist <= shapeRadius) {
 		hit++;
+		dataPoint.hit = true;
 	} else {
 		missed++;
 	}
+	data.hitData.push(dataPoint);
 	generateNewShape();
 }
 
@@ -92,3 +141,56 @@ function draw() {
 }
 
 setInterval(draw,1000/60);
+
+function getData() {
+	data.hit = hit;
+	data.missed = missed;
+	data.total = hit+missed;
+	// document.getElementById("data").value = JSON.stringify(data,null,2);
+	document.getElementById("data").value = getCSV();
+}
+
+/**
+ * 
+ * @param {DataPoint} d 
+ * @returns {Object.<string,any>}
+ */
+function flattenDataPoint(d) {
+	return {
+		time: d.time,
+		hit: d.hit,
+		shapeX: d.shape.x,
+		shapeY: d.shape.y,
+		shapeRadius: d.shape.radius,
+		shapeR: d.shape.color[0],
+		shapeG: d.shape.color[1],
+		shapeB: d.shape.color[2],
+		bgR: d.bg[0],
+		bgG: d.bg[1],
+		bgB: d.bg[2],
+	}
+}
+
+function getCSV() {
+	let dataPoints = [];
+	let text = "";
+	for (let d of data.hitData) {
+		dataPoints.push(flattenDataPoint(d));
+	}
+	if (dataPoints.length) {
+		let feilds = Object.keys(dataPoints[0]);
+		for (let f of feilds) {
+			text += `${f},`;
+		}
+		text += "\n";
+		// text = text.slice(0,text.length-2); // remove last comma
+		for (let d of dataPoints) {
+			for (let f of feilds) {
+				text += `${d[f]},`
+			}
+			text += "\n";
+		}
+	}
+
+	return text;
+}
